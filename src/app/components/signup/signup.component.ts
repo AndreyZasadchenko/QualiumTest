93005b52639fa6de2b16365cb76743c2a6cce37f
Andrey Zasadchenko <runnermail.root@gmail.com>
2017-05-04 13:50:48 +0300

    fix(signup) auth user after reg/redirect to home

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -12,13 +12,31 @@ import { AuthService } from '../../services/auth/auth.service';
 export class SignupComponent {
   constructor(public router: Router, public http: Http, private auth: AuthService) {}
 
+  error: String;
+
   signup(event, email, password) {
     event.preventDefault();
+    this.error = "";
     let body = JSON.stringify({ email, password });
     this.http.post('http://localhost:9000/users', body, { headers: contentHeaders })
       .subscribe(
         response => {
-          this.auth.login(email, password);
+          this.auth.login(email, password).subscribe(
+            response => {
+              this.auth.token = response.json().token;
+              localStorage.setItem('user', response.json().user.id);
+              localStorage.setItem('token', this.auth.token);
+              localStorage.setItem('email', response.json().user.email);
+              this.router.navigate(['']);
+            },
+            error => {
+              if (error.status === 401) {
+                this.error = "Invalid email/password or unauthorized";
+                return;
+              }
+            },
+            () => console.log('login#done')
+          );
         },
         error => {
           alert(error.text());
